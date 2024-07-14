890f2c6d668da26dbe35267be03ac5f3f8857d79
Nikhil <Nikhilgargcoder@gmail.com>
2024-07-14 17:12:44 +0530

    Sandboxing

--- index.ts
+++ index.ts
@@ -62,6 +62,10 @@ export const handler = async (): Promise<any> => {
       concurrency: Cluster.CONCURRENCY_CONTEXT,
       maxConcurrency: 2,
       puppeteer,
+      puppeteerOptions:{
+        args:["--no-sandbox",
+        "--disable-setuid-sandbox",]
+      }
     });
 
     const response = await getS3Object("Blinkit Area.csv");
@@ -76,7 +80,7 @@ export const handler = async (): Promise<any> => {
             "--disable-client-side-phishing-detection",
             "--disable-software-rasterizer",
             "--disable-dev-shm-usage",
-            "--proxy-server=103.162.133.224:49155"
+            "--proxy-server=103.162.133.224:49155",    
 
           ],
         
