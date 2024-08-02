8af6e1f4b497dabed485ef3f8471f446ac649c24
Nikhil <Nikhilgargcoder@gmail.com>
2024-08-02 15:47:54 +0530

    Minor changes

--- index.ts
+++ index.ts
@@ -66,7 +66,7 @@ export const handler = async (): Promise<any> => {
     const cluster = await Cluster.launch({
       puppeteer,
       concurrency: Cluster.CONCURRENCY_CONTEXT,
-      maxConcurrency: 100,
+      maxConcurrency: parseInt(process.env.PUPPETEER_MAXCONCURRENCY || "2"),
       
       puppeteerOptions: {
         defaultViewport: null,
