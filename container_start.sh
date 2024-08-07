0deaadafc302bb3563ba237e9db1bf220cf6cda8
Nikhil <Nikhilgargcoder@gmail.com>
2024-08-07 16:20:28 +0530

    maximum number of clients reached

--- container_start.sh
+++ container_start.sh
@@ -2,7 +2,7 @@
 
 echo "starting X server and VNC display"
 touch ~/.Xauthority
-Xvfb :1 -screen 0 1024x768x24 &
+Xvfb :1 -screen 0 1024x768x24 -maxclients 2048 &
 /usr/bin/x11vnc -display :1.0 -usepw -quiet &
 DISPLAY=:1.0
 export DISPLAY
